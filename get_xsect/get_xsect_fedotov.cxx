5bed5dac848f231d3a76206f445c6321efb49d0a
skorodumina <skorodumina@gmail.com>
2020-11-20 19:30:59 -0500

    get_xsect clean up

--- get_xsect/get_xsect_fedotov.cxx
+++ get_xsect/get_xsect_fedotov.cxx
@@ -10,70 +10,81 @@
 
 using namespace std;
 
-
 Short_t getWbin_fed (Float_t W) {
-//return int(W*10000. - 1.4125*10000.)/250;
-//return int((W-1.4125)/0.025);
+Short_t bin;
 
-if ((W>=1.3125)&&(W<=1.3375)) return 0;
-if ((W>=1.3375)&&(W<=1.3625)) return 1;
-if ((W>=1.3625)&&(W<=1.3875)) return 2;
-if ((W>=1.3875)&&(W<=1.4125)) return 3;
-if ((W>=1.4125)&&(W<=1.4375)) return 4;
-if ((W>=1.4375)&&(W<=1.4625)) return 5;
-if ((W>=1.4625)&&(W<=1.4875)) return 6;
-if ((W>=1.4875)&&(W<=1.5125)) return 7;
+if ((W>=1.3125)&&(W<=1.3375)) bin = 0;
+if ((W>=1.3375)&&(W<=1.3625)) bin = 1;
+if ((W>=1.3625)&&(W<=1.3875)) bin = 2;
+if ((W>=1.3875)&&(W<=1.4125)) bin = 3;
+if ((W>=1.4125)&&(W<=1.4375)) bin = 4;
+if ((W>=1.4375)&&(W<=1.4625)) bin = 5;
+if ((W>=1.4625)&&(W<=1.4875)) bin = 6;
+if ((W>=1.4875)&&(W<=1.5125)) bin = 7;
 
-if ((W>=1.5125)&&(W<=1.5375)) return 8;
-if ((W>=1.5375)&&(W<=1.5625)) return 9;
-if ((W>=1.5625)&&(W<=1.5875)) return 10;
-
-
+if ((W>=1.5125)&&(W<=1.5375)) bin = 8;
+if ((W>=1.5375)&&(W<=1.5625)) bin = 9;
+if ((W>=1.5625)&&(W<=1.5875)) bin = 10;
 
 if ((W<1.3125)||(W>1.5875)) {
-cout << "Error, wrong W range "  <<W<< "\n";
-return -100;
+cout << "Error, wrong W range, Fedotov" << "\n";
+bin = -100;
 };
+return bin;
 };
 //-------
 Short_t getQ2bin_fed (Float_t Q2) {
-if ((Q2 >= 0.225) && (Q2 <= 0.275)) return 0;
-if ((Q2 >= 0.275) && (Q2 <= 0.325)) return 1;
-if ((Q2 >= 0.325) && (Q2 <= 0.425)) return 2;
 
-if ((Q2 >= 0.425) && (Q2 <= 0.475)) return 3;
-if ((Q2 >= 0.475) && (Q2 <= 0.525)) return 4;
-if ((Q2 >= 0.525) && (Q2 <= 0.575)) return 5;
+Short_t bin;
+if ((Q2 >= 0.225) && (Q2 <= 0.275)) bin = 0;
+if ((Q2 >= 0.275) && (Q2 <= 0.325)) bin = 1;
+if ((Q2 >= 0.325) && (Q2 <= 0.425)) bin = 2;
+
+if ((Q2 >= 0.425) && (Q2 <= 0.475)) bin = 3;
+if ((Q2 >= 0.475) && (Q2 <= 0.525)) bin = 4;
+if ((Q2 >= 0.525) && (Q2 <= 0.575)) bin = 5;
 
 if ((Q2 < 0.225) || (Q2 > 0.575)) {
-cout << "Error, wrong fed Q2 range" << "\n";
-return -100;
+cout << "Error, wrong Q2 range, Fedotov" << "\n";
+bin = -100;
 };
+return bin;
 };
+
 //---------
 Short_t getsbin_fed (Short_t Wbin, Float_t sgen, Float_t Smax, Float_t Smin) {
-if ((sgen>=Smin)&&(sgen<=Smax)) return int((sgen-Smin)/((Smax - Smin)/9.));
-if (sgen<Smin) return 0;
-if (sgen>Smax) return 8;
 
+Short_t bin;
+if ((sgen>=Smin)&&(sgen<=Smax)) bin = int((sgen-Smin)/((Smax - Smin)/9.));
+if (sgen<Smin) bin = 0;
+if (sgen>Smax) bin = 8;
 
-
+return bin;
 };
 //--------------------
 
 Short_t getanglebin_fed (Float_t anglegen, Float_t anglemax) {
 
-if ((anglegen < 0.01)) return 0;
-if ((anglegen > anglemax - 0.01)) return 6;
-if ((anglegen >= 0.01) && (anglegen <= anglemax - 0.01)) return int((anglegen - 0.01)/((anglemax  - 0.02)/7.));
+Short_t bin;
+if ((anglegen < 0.01)) bin = 0;
+if ((anglegen > anglemax - 0.01)) bin = 6;
+if ((anglegen >= 0.01) && (anglegen <= anglemax - 0.01)) bin = int((anglegen - 0.01)/((anglemax  - 0.02)/7.));
 
-
+return bin;
 };
 
 
+//This subroutine gets Fedotov cross sections on the tabuted grid and interpolates them to a desired point within the region covered by Fedotov cross section
+//This is the grid and xsect arrays for Fedotov cross sections:
+//W_ARR_FED[12];
+//Q2_ARR_FED[7];
+//S12_ARR_FED[10][12];
+//S23_ARR_FED[10][12];
+//THETA_ARR_FED[8]; 
+//ALPHA_ARR_FED[8];
+//SIGMA_ARR_FED[6][7][12][10][10][8][8];
 
-
-//This subrouting is doing the following:
+//The subrouting is doing the following:
 //1 - using auxiliary functions (getWbin, getQ2bin, getsbin, getanglebin) we identify the number of left and right point 
 //2 - then we are doing 4d-interpolation for each (Wleft_bin, Q2_left_bin),  (Wright_bin, Q2_left_bin), (Wright_bin, Q2_right_bin) and (Wleft_bin, Q2_right_bin) and obtain cross-secton in that points (4 GLOBAL 6dim arrays)
 //3 - then we are doing 2d-interpolation for the points written above and obtain sigma_final[6]
@@ -98,8 +109,6 @@ Short_t s12right_wleft_bin = s12left_wleft_bin +1;
 Short_t s12left_wright_bin = getsbin_fed(Wright_bin, s12gen, S12_ARR_FED[9][Wright_bin], S12_ARR_FED[0][Wright_bin]);
 Short_t s12right_wright_bin = s12left_wright_bin +1;
 
-
-
 Short_t s23left_wleft_bin = getsbin_fed(Wleft_bin, s23gen, S23_ARR_FED[9][Wleft_bin], S23_ARR_FED[0][Wleft_bin]);
 Short_t s23right_wleft_bin = s23left_wleft_bin +1;
 
@@ -115,6 +124,8 @@ Short_t thetaright_bin = thetaleft_bin+1;
 Short_t alphaleft_bin = getanglebin_fed(alphagen,ALPHA_ARR_FED[7]);
 Short_t alpharight_bin = alphaleft_bin+1;
 
+//cout << alphagen<<" "<< alphaleft_bin<<" "<< alpharight_bin<<" \n";
+
 Float_t sigma_final[6];
 
 for (Short_t i=0;i<6;i++){
@@ -125,12 +136,12 @@ interpol_fedotov(4,Q2right_bin,Wright_bin,s12left_wright_bin,s12right_wright_bin
 interpol_fedotov(4,Q2right_bin,Wleft_bin,s12left_wleft_bin,s12right_wleft_bin,s23left_wleft_bin,s23right_wleft_bin,thetaleft_bin,thetaright_bin,alphaleft_bin,alpharight_bin,s12gen,s23gen,thetagen,alphagen,sigma_wleft_q2right[i],i);
 
 interpol_fedotov(4,Q2left_bin,Wleft_bin,s12left_wleft_bin,s12right_wleft_bin,s23left_wleft_bin,s23right_wleft_bin,thetaleft_bin,thetaright_bin,alphaleft_bin,alpharight_bin,s12gen,s23gen,thetagen,alphagen,sigma_wleft_q2left[i],i);
-
 };
 
 for (Short_t i=0;i<6;i++){
 interpol_fedotov(2,0,0, Wleft_bin, Wright_bin, Q2left_bin, Q2right_bin, 0,  0, 0,  0, Wgen, Q2gen, 0., 0.,sigma_final[i], i);
 };
+
 //We get explicitly different sigmas from the array
  sigma_t_final = sigma_final[0];
  sigma_l_final = sigma_final[1];
@@ -138,6 +149,5 @@ interpol_fedotov(2,0,0, Wleft_bin, Wright_bin, Q2left_bin, Q2right_bin, 0,  0, 0
  sigma_s2f_final = sigma_final[3];
  sigma_cf_final = sigma_final[4];
  sigma_sf_final = sigma_final[5];
-
 
 };
--- get_xsect/get_xsect_golovach.cxx
+++ get_xsect/get_xsect_golovach.cxx
@@ -6,92 +6,109 @@
 #include <iostream>
 #include "global.h"
 #include "interpol_golovach.h"
-
-
+#include "interpol_gol2.h"
+#include "get_xsect_ripani.h"
 using namespace std;
-
-
-
 
 //-------
 Short_t getWbin_GOL (Float_t W) {
-//return int(W*10000. - 1.4125*10000.)/250;
-if ((W>=1.6125)&&(W<=2.1375))  return int((W-1.6125)/0.025);
-if ((W>2.1375)&&(W<=2.5375))  return 21+int((W-2.1375)/0.05);
+Short_t bin;
+if ((W>=1.6125)&&(W<=2.1375))  bin = int((W-1.6125)/0.025);
+if ((W>2.1375)&&(W<=2.5375))  bin = 21+int((W-2.1375)/0.05);
 
 /*
+if ((W>=1.6125)&&(W<=1.6375)) bin = 0;
+if ((W>=1.6375)&&(W<=1.6625)) bin = 1;
+if ((W>=1.6625)&&(W<=1.6875)) bin = 2;
+if ((W>=1.6875)&&(W<=1.7125)) bin = 3;
 
-if ((W>=1.6125)&&(W<=1.6375)) return 0;
-if ((W>=1.6375)&&(W<=1.6625)) return 1;
-if ((W>=1.6625)&&(W<=1.6875)) return 2;
-if ((W>=1.6875)&&(W<=1.7125)) return 3;
+if ((W>=1.7125)&&(W<=1.7375)) bin = 4;
+if ((W>=1.7375)&&(W<=1.7625)) bin = 5;
+if ((W>=1.7625)&&(W<=1.7875)) bin = 6;
+if ((W>=1.7875)&&(W<=1.8125)) bin = 7;
 
-if ((W>=1.7125)&&(W<=1.7375)) return 4;
-if ((W>=1.7375)&&(W<=1.7625)) return 5;
-if ((W>=1.7625)&&(W<=1.7875)) return 6;
-if ((W>=1.7875)&&(W<=1.8125)) return 7;
+if ((W>=1.8125)&&(W<=1.8375)) bin = 8;
+if ((W>=1.8375)&&(W<=1.8625)) bin = 9;
+if ((W>=1.8625)&&(W<=1.8875)) bin = 10;
+if ((W>=1.8875)&&(W<=1.9125)) bin = 11;
 
-if ((W>=1.8125)&&(W<=1.8375)) return 8;
-if ((W>=1.8375)&&(W<=1.8625)) return 9;
-if ((W>=1.8625)&&(W<=1.8875)) return 10;
-if ((W>=1.8875)&&(W<=1.9125)) return 11;
+if ((W>=1.9125)&&(W<=1.9375)) bin = 12;
+if ((W>=1.9375)&&(W<=1.9625)) bin = 13;
+if ((W>=1.9625)&&(W<=1.9875)) bin = 14;
+if ((W>=1.9875)&&(W<=2.0125)) bin = 15;
 
-if ((W>=1.9125)&&(W<=1.9375)) return 12;
-if ((W>=1.9375)&&(W<=1.9625)) return 13;
-if ((W>=1.9625)&&(W<=1.9875)) return 14;
-if ((W>=1.9875)&&(W<=2.0125)) return 15;
+if ((W>=2.0125)&&(W<=2.0375)) bin = 16;
+if ((W>=2.0375)&&(W<=2.0625)) bin = 17;
+if ((W>=2.0625)&&(W<=2.0875)) bin = 18;
+if ((W>=2.0875)&&(W<=2.1125)) bin = 19;
 
-if ((W>=2.0125)&&(W<=2.0375)) return 16;
-if ((W>=2.0375)&&(W<=2.0625)) return 17;
-if ((W>=2.0625)&&(W<=2.0875)) return 18;
-if ((W>=2.0875)&&(W<=2.1125)) return 19;
+if ((W>=2.1125)&&(W<=2.1375)) bin = 20;
 
-if ((W>=2.1125)&&(W<=2.1375)) return 20;
-
-if ((W>=2.1375)&&(W<=2.1875)) return 21;
-if ((W>=2.1875)&&(W<=2.2375)) return 22;
-if ((W>=2.2375)&&(W<=2.2875)) return 23;
-if ((W>=2.2875)&&(W<=2.3375)) return 24;
-if ((W>=2.3375)&&(W<=2.3875)) return 25;
-if ((W>=2.3875)&&(W<=2.4375)) return 26;
-if ((W>=2.4375)&&(W<=2.4875)) return 27;
-if ((W>=2.4875)&&(W<=2.5375)) return 28;
-
+if ((W>=2.1375)&&(W<=2.1875)) bin = 21;
+if ((W>=2.1875)&&(W<=2.2375)) bin = 22;
+if ((W>=2.2375)&&(W<=2.2875)) bin = 23;
+if ((W>=2.2875)&&(W<=2.3375)) bin = 24;
+if ((W>=2.3375)&&(W<=2.3875)) bin = 25;
+if ((W>=2.3875)&&(W<=2.4375)) bin = 26;
+if ((W>=2.4375)&&(W<=2.4875)) bin = 27;
+if ((W>=2.4875)&&(W<=2.5375)) bin = 28;
 */
 if ((W<1.6125)||(W>2.5375)) {
-cout << "Error, wrong W range" << "\n";
-return -100;
-}
-
+cout << "Error, wrong W range, Golovach" << "\n";
+bin = -100;
+};
+return bin;
 };
 
+//-------
+Short_t getWbin_GOL2 (Float_t W) {
+
+Short_t bin;
+if ((W>2.5875)&&(W<=3.0375))  bin = int((W-2.5875)/0.05);
+
+if ((W<2.5875)||(W>3.0375)) {
+cout << "Error, wrong W range, WGOL2" << "\n";
+bin = -100;
+}
+return bin;
+};
 
 //---------
 Short_t getsbin_GOL (Float_t sgen, Float_t Smax, Float_t Smin) {
-if ((sgen>=Smin)&&(sgen<=Smax)) return int((sgen-Smin)/((Smax - Smin)/15.));
-if (sgen<Smin) return 0;
-if (sgen>Smax) return 14;
 
+Short_t bin;
+if ((sgen>=Smin)&&(sgen<=Smax)) bin = int((sgen-Smin)/((Smax - Smin)/15.));
+if (sgen<Smin) bin = 0;
+if (sgen>Smax) bin = 14;
 
+return bin;
 };
 //--------------------
 Short_t getanglebin_GOL (Float_t anglegen, Float_t anglemax) {
 
-if ((anglegen < 0.01)) return 0;
-if ((anglegen > anglemax - 0.01)) return 12;
-if ((anglegen >= 0.01) && (anglegen <= anglemax - 0.01)) return int((anglegen - 0.01)/((anglemax  - 0.02)/13.));
+Short_t bin;
+if ((anglegen < 0.01)) bin = 0;
+if ((anglegen > anglemax - 0.01)) bin = 12;
+if ((anglegen >= 0.01) && (anglegen <= anglemax - 0.01)) bin = int((anglegen - 0.01)/((anglemax  - 0.02)/13.));
 
-
+return bin;
 };
 
+//This file contains two similar subroutine for geting photoproduction cross sections.
+//get_xsect_gol_datamod(...) - gets Golovach cross sections for W from 1.6125 to 2.5375 GeV
+//get_xsect_gol_model(...) - takes cross sections in the region W from 2.5875 to 3.0375 GeV and Q2 = 0 GeV^2 (pure-model based)
 
-//This subrouting is doing the following:
-//1 - using auxiliary functions (getWbin_GOL, getsbin_GOL, getanglebin_GOL) we identify the left and right point for each variable generated  (according to Golovach binning)
-//2 - then we are doing 4d-interpolation for each Wleft_bin and Wright_bin and obtain sigma_wright_gol and sigma_wleft_gol
+//This subroutine gets Golovach cross sections on the tabuted grid and interpolates them to a desired point within W from 1.6125 to 2.5375 GeV
+//This is the grid and xsect arrays for Golovach cross sections:
+//W_ARR_GOL[30];
+//S12_ARR_GOL[16][30];
+//S23_ARR_GOL[16][30];
+//THETA_ARR_GOL[14]; 
+//ALPHA_ARR_GOL[14];
+//SIGMA_ARR_GOL[30][16][16][14][14];
+ 
 
-
-void get_xsect_golovach(Float_t Wgen, Float_t s12gen,Float_t s23gen, Float_t thetagen, Float_t alphagen, Short_t &Wleft_bin, Float_t &sigma_wright_gol,Float_t &sigma_wleft_gol){ 
-
+void get_xsect_gol_datamod(Float_t Wgen, Float_t s12gen,Float_t s23gen, Float_t thetagen, Float_t alphagen, Short_t &Wleft_bin, Float_t &sigma_wright_gol,Float_t &sigma_wleft_gol){ 
 
 //using auxiliary functions (getWbin_GOL, getsbin_GOL, getanglebin_GOL) we identify the left and right point for each variable generated  (according to Golovach binning)
 Wleft_bin = getWbin_GOL(Wgen);
@@ -112,31 +129,61 @@ Short_t s23right_wleft_bin = s23left_wleft_bin +1;
 Short_t s23left_wright_bin = getsbin_GOL(s23gen, S23_ARR_GOL[15][Wright_bin], S23_ARR_GOL[0][Wright_bin]);
 Short_t s23right_wright_bin = s23left_wright_bin +1;
 
+//cout << s23left_wleft_bin <<" "<< Wgen << " "<<Wleft_bin<< " " << s23gen << " "<< S23_ARR_GOL[s23left_wleft_bin][Wleft_bin] << " "<< S23_ARR_GOL[s23right_wleft_bin][Wleft_bin] << "\n";
+
 Short_t thetaleft_bin = getanglebin_GOL(thetagen,THETA_ARR_GOL[13]);
 Short_t thetaright_bin = thetaleft_bin+1;
 
 Short_t alphaleft_bin = getanglebin_GOL(alphagen,ALPHA_ARR_GOL[13]);
 Short_t alpharight_bin = alphaleft_bin+1;
-//cout << s23left_wleft_bin <<" "<< Wgen << " "<<Wleft_bin<< " " << s23gen << " "<< S23_ARR_GOL[s23left_wleft_bin][Wleft_bin] << " "<< S23_ARR_GOL[s23right_wleft_bin][Wleft_bin] << "\n";
+
+//cout << thetagen <<" "<< thetaleft_bin<<" "<< thetaright_bin<<" \n";
 
 //then we are doing 4d-interpolation for each Wleft_bin and Wright_bin and obtain sigma_wright_gol and sigma_wleft_gol
 interpol_golovach(Wright_bin,s12left_wright_bin,s12right_wright_bin,s23left_wright_bin,s23right_wright_bin,thetaleft_bin,thetaright_bin,alphaleft_bin,alpharight_bin,s12gen,s23gen,thetagen,alphagen,sigma_wright_gol);
 
 interpol_golovach(Wleft_bin,s12left_wleft_bin,s12right_wleft_bin,s23left_wleft_bin,s23right_wleft_bin,thetaleft_bin,thetaright_bin,alphaleft_bin,alpharight_bin,s12gen,s23gen,thetagen,alphagen,sigma_wleft_gol);
-
-//cout <<Wgen<<" "<< sigma_wright_gol << " "<<sigma_wleft_gol<<"\n";
-
-//cout << Wgen << " "<< s12left_wright_bin << " "<<s12right_wright_bin<<" "<<S12_ARR_GOL[s12right_wright_bin][Wright_bin] << "  "<<s23left_wright_bin<< " "<< s23right_wright_bin<< " "<< sigma_wright_gol<<"\n";
-//cout << Wgen << " "<< s12left_wleft_bin << " "<<s12right_wleft_bin<<" "<<S12_ARR_GOL[s12right_wleft_bin][Wleft_bin]<< "  "<<s23left_wleft_bin<< " "<< s23right_wleft_bin<< " "<< sigma_wleft_gol<<"\n";
-
-//for (Short_t i=0;i<=13;i++){
-
-//cout<< i<<" "<<S12_ARR[i][2]<<"\n";
-//};
-
-
-//sigma_t_gol = 1./fabs(W_ARR_GOL[Wright_bin]-W_ARR_GOL[Wleft_bin]);
-//sigma_t_gol = sigma_t_gol*(sigma_wright_gol*fabs(W_ARR_GOL[Wleft_bin]-Wgen)+sigma_wleft_gol*fabs(W_ARR_GOL[Wright_bin]-Wgen));
-//cout << sigma_t_gol << " "<< sigma_wright_gol << " "<< sigma_wleft_gol<<"\n";
-return;
 };
+
+
+//This subroutine takes cross sections in the region W from 2.5875 to 3.0375 GeV and Q2 = 0 GeV^2 (pure-model based)
+//This is the grid and xsect arrays for this cross sections:
+//W_ARR_RIP3[10];
+//S12_ARR_RIP3[16][10];
+//S23_ARR_RIP3[16][10];
+//THETA_ARR[6]; 
+//ALPHA_ARR[6];
+//SIGMA_ARR_GOL2[10][16][16][6][6]; (the same as sigma_t from SIGMA_ARR_RIP3[6][10][16][16][6][6], but with scaling factors, which are W, s12/s23, and angles dependent)
+
+void get_xsect_gol_model(Float_t Wgen, Float_t s12gen,Float_t s23gen, Float_t thetagen, Float_t alphagen, Short_t &Wleft_bin, Float_t &sigma_wright_gol,Float_t &sigma_wleft_gol){ 
+
+Wleft_bin = getWbin_GOL2(Wgen);
+Short_t Wright_bin = Wleft_bin+1;
+//cout << Wgen << " "<< Wleft_bin<< "\n";
+
+Short_t s12left_wleft_bin = getsbin_GOL(s12gen, S12_ARR_RIP3[15][Wleft_bin], S12_ARR_RIP3[0][Wleft_bin]);
+Short_t  s12right_wleft_bin = s12left_wleft_bin +1;
+
+Short_t  s12left_wright_bin = getsbin_GOL(s12gen, S12_ARR_RIP3[15][Wright_bin], S12_ARR_RIP3[0][Wright_bin]);
+Short_t  s12right_wright_bin = s12left_wright_bin +1;
+
+Short_t  s23left_wleft_bin = getsbin_GOL(s23gen, S23_ARR_RIP3[15][Wleft_bin], S23_ARR_RIP3[0][Wleft_bin]);
+Short_t  s23right_wleft_bin = s23left_wleft_bin +1;
+
+Short_t  s23left_wright_bin = getsbin_GOL(s23gen, S23_ARR_RIP3[15][Wright_bin], S23_ARR_RIP3[0][Wright_bin]);
+Short_t  s23right_wright_bin = s23left_wright_bin +1;
+
+//cout << s23left_wleft_bin <<" "<< Wgen << " "<<Wleft_bin<< " " << s23gen << " "<< S23_ARR_RIP3[s23left_wleft_bin][Wleft_bin] << " "<< S23_ARR_RIP3[s23right_wleft_bin][Wleft_bin] << "\n";
+
+Short_t thetaleft_bin = getanglebin(thetagen,THETA_ARR[5]);
+Short_t thetaright_bin = thetaleft_bin+1;
+
+Short_t alphaleft_bin = getanglebin(alphagen,ALPHA_ARR[5]);
+Short_t alpharight_bin = alphaleft_bin+1;
+
+//then we are doing 4d-interpolation for each Wleft_bin and Wright_bin and obtain sigma_wright_gol and sigma_wleft_gol
+interpol_gol2(Wright_bin,s12left_wright_bin,s12right_wright_bin,s23left_wright_bin,s23right_wright_bin,thetaleft_bin,thetaright_bin,alphaleft_bin,alpharight_bin,s12gen,s23gen,thetagen,alphagen,sigma_wright_gol);
+
+interpol_gol2(Wleft_bin,s12left_wleft_bin,s12right_wleft_bin,s23left_wleft_bin,s23right_wleft_bin,thetaleft_bin,thetaright_bin,alphaleft_bin,alpharight_bin,s12gen,s23gen,thetagen,alphagen,sigma_wleft_gol);
+};
+
